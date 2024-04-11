40ecfc5c6ee35e372b8739fcb3b6f27660a68660
MarkoTadic99 <nekicudanusername@gmail.com>
2024-04-12 00:34:06 +0200

    Tailwind.css setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to TaxiApp</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-red-500 text-white p-4">
+      Ovo je testiranje Tailwind CSS-a.
     </div>
   );
 }
